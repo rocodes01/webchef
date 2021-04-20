13d7b9473c9e67d257b32cadb94668299f045871
rohitsingh71 <rohit.kumar@espranza.in>
2021-04-20 22:13:15 +0530

    Random changes

---
+++ js/coding_page.js
@@ -0,0 +1,33 @@
+const contest_name = localStorage.getItem("contest_name");
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+let contestsRef = firebase.firestore().collection("contests");
+
+function updatecollection() {
+  const htmlcode = document.getElementById("html").value;
+  const csscode = document.getElementById("css").value;
+  const finalCode = `<head>
+      <style>
+      ${csscode}
+      </style>
+      </head>
+      <body>
+      ${htmlcode}
+      </body>
+      `;
+  contest = contestsRef
+    .doc("new")
+    .set({ sub: finalCode })
+    .then((res) => {
+      console.log(res);
+    });
+}
+function saveCodeToDB() {
+  contest = contestsRef
+    .where("name", "==", contest_name)
+    .get()
+    .then((snapshot) => {
+      updatecollection(snapshot.docs[0].data());
+    });
+}
