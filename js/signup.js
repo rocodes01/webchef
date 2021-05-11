95927e7cf8112491dc329139a5373d88ef20e072
rohitsingh71 <rohit.kumar@espranza.in>
2021-05-11 23:48:41 +0530

    form Validations Added

--- js/signup.js
+++ js/signup.js
@@ -1,37 +1,63 @@
 const db = firebase.firestore();
+const showToast = (type, message) => {
+  const toast = document.getElementById("toast");
+  const toastBody = document.getElementById("toastBody");
+  toastBody.innerText = message;
+  toast.style.opacity = 1;
+  type.classList.add("errorInput");
+  setTimeout(() => {
+    type.classList.remove("errorInput");
+    toast.style.opacity = 0;
+  }, 2000);
+};
 const signup = (event) => {
   event.preventDefault();
-  const email = document.getElementById("InputEmail").value;
-  const password = document.getElementById("InputPassword").value;
-  const usertype = document.getElementById("usertype").value;
-  const first = document.getElementById("FirstName").value;
-  const last = document.getElementById("LastName").value;
-  console.log(email, password, usertype);
+  const email = document.getElementById("InputEmail");
+  const password = document.getElementById("InputPassword");
+  const repassword = document.getElementById("RepeatPassword");
+  const usertype = document.getElementById("usertype");
+  const first = document.getElementById("FirstName");
+  const last = document.getElementById("LastName");
+  console.log(email.value, password.value, usertype.value);
   const userdata = {
-    name: `${first} ${last}`,
-    email: email,
-    role: Number(usertype),
+    name: `${first.value} ${last.value}`,
+    email: email.value,
+    role: Number(usertype.value),
   };
   console.log(userdata);
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      var user = userCredential.user;
-      db.collection("users")
-        .doc(user.uid)
-        .set(userdata)
-        .then((res) => {
-          console.log(" res", res);
+  if (!email.value.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")) {
+    showToast(email, "email format Not Valid");
+  } else if (first.value.length <= 2) {
+    showToast(first, "First name Should be greater than 2 characters");
+  } else if (last.value.length <= 2) {
+    showToast(last, "Last name Should be greater than 2 characters");
+  } else if (isNaN(usertype.value)) {
+    showToast(usertype, "Please Select a User Type");
+  } else if (password.value.length < 6) {
+    showToast(password, "Password Length cannot be less than 6");
+  } else if (password.value != repassword.value) {
+    showToast(repassword, "Passwords Do not match");
+  } else {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email.value, password.value)
+      .then((userCredential) => {
+        var user = userCredential.user;
+        db.collection("users")
+          .doc(user.uid)
+          .set(userdata)
+          .then((res) => {
+            console.log(" res", res);
 
-          window.location.href = "index.html";
-        });
-      console.log(user.uid);
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ..
-      console.log(errorCode, ":", errorMessage);
-    });
+            window.location.href = "index.html";
+          });
+        console.log(user.uid);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+        console.log(errorCode, ":", errorMessage);
+      });
+  }
 };
